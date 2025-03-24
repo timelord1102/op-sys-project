d1c6fe983b9fe622b2452c596f6f78c634e89dfc
montmaj <timelord1102@gmail.com>
2025-03-23 21:45:20 -0400

    minor fixes

--- test.sh
+++ test.sh
@@ -20,16 +20,16 @@
 # python3 main.py 8 6 768 0.001 1024 6 .95 128 > test.txt
 # diff -w test.txt 03_sjf.txt
 
-# python3 main.py 16 2 256 0.001 2048 4 .45 32 > test.txt
+#python3 main.py 16 2 256 0.001 2048 4 .45 32 > test.txt
 # diff -w test.txt 04_sjf.txt
 
-# python3 main.py 20 16 128 0.01 4096 4 0.99 64 > test.txt
+python3 main.py 20 16 128 0.01 4096 4 0.99 64 > test.txt
 # diff -w test.txt 05_sjf.txt
 
 # ---------------------------SRT------------------------------
 # python3 main.py 3 1 32 0.001 1024 4 .75 256 > test.txt
 
-python3 main.py 8 6 768 0.001 1024 6 .95 128 > test.txt
+# python3 main.py 8 6 768 0.001 1024 6 .95 128 > test.txt
 
 
 
